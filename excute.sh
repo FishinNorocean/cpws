b4d753f3faf0976a6fbb699de94af3d89d6d5b8c
swk <swklouis@gmail.com>
2023-11-28 20:04:36 +0800

    version 1.0.3-beta

---
+++ excute.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Initializing"
+directory="./results"  
+if [ ! -d "$directory" ]; then
+    echo "./results directory isn't here. Setting it up..."
+    mkdir -p "$directory" 
+    echo "Done"
+else
+    echo "./results directory already exists"
+fi
+
+sbatch run.sh
--- run.sh
+++ run.sh
@@ -7,14 +7,4 @@
 #SBATCH -o results/Out.txt
 #SBATCH -e results/Error.txt
 export PATH=/share/apps/cuda-12.1/bin:/share/apps/anaconda3/py385-2020.11/bin:$PATH 
-
-echo "Initializing"
-directory="./results"  
-if [ ! -d "$directory" ]; then
-    echo "./results directory isn't here. Setting it up..."
-    mkdir -p "$directory" 
-    echo "Done"
-else
-    echo "./results directory already exists"
-fi
 python x.py

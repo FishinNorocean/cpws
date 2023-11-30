ecd3d4567b62e1b31369a4043209ee727d77f843
swk <swklouis@gmail.com>
2023-11-30 21:23:21 +0800

    monitor bug fixed

--- monitor.sh
+++ monitor.sh
@@ -3,5 +3,5 @@ running_job_id=$(squeue | grep ' R ' | awk '{print $1}')
 if [ -z "$running_job_id" ]; then
     echo "No working jobs."
 else
-    watch -n 1 "sacct -u zju0018053 -j \"$running_job_id\" --field=jobid,jobname,nodelist,elapsed,state; tail -n 20 gpu_status.log; echo \"main.log:\"; cat results/main.log; echo \"Error:\";cat results/Error.txt"
+    watch -n 1 "sacct -u zju0018053 -j $running_job_id\ --field=jobid,jobname,nodelist,elapsed,state; tail -n 20 gpu_status.log; echo \"main.log:\"; cat results/main.log; echo \"Error:\";cat results/Error.txt"
 fi

eb6803499e785207b2e161e293822fa2564054a6
swk <swklouis@gmail.com>
2023-12-06 16:08:08 +0800

    excute.sh ignore modifications added two more.

--- gender_result_procession/execute.sh
+++ gender_result_procession/execute.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Initializing..."
-cd "$HOME/swk_cpws_1.0/gender_result_procession"
-git update-index --assume-unchanged acu_main.log Error.txt gpu_status.log id.sh mission_args.txt Out.txt parameters.txt log/.gitkeep results/.gitkeep
+git update-index --assume-unchanged acu_main.log Error.txt gpu_status.log id.sh mission_args.txt Out.txt parameters.txt log/.gitkeep results/.gitkeep ../Models/.gitkeep ../Pre_data/.gitkeep
 touch acu_main.log
 mkdir -p acu_results
 echo "Done."
